c751382772fb16712baa8edc8b4f4ced9d0a7d20
Luxad <l.abromavicius@gmail.com>
2025-01-18 18:33:53 +0200

    url fix

--- ksn/urls.py
+++ ksn/urls.py
@@ -8,4 +8,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("ckeditor5/", include('django_ckeditor_5.urls')),
     path('api/web/', include('web.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
